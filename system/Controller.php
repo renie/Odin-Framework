29fd8e6ed84dbd89ac114356a8c9aa1541624942
Renie Siqueira <renie.webdev@gmail.com>
2012-01-10 13:43:07 -0200

    Change the way parameters must to be passed to view.

--- system/Controller.php
+++ system/Controller.php
@@ -12,31 +12,53 @@
     class Controller extends System{
         
         /**
+         * Stores parameters
+         * @access private
+         * @var array 
+         */
+        private $vars;
+        
+        /**
          * Loads required view and treat the parameters.
          * 
-         * Receives name of View and variables will be available into.
-         * If variable is an array, it will be broken in indepentent variables
-         * that will be named with the key concatenated with string "view". Therefore
+         * Receives name of View.
+         * Parameters must to be setted on {setVars} method.
+         * {vars} will be broken in indepentent variables
+         * that will be named with the key concatenated with string "view_". Therefore
          * an array like array("name"=>"foo","id"=>0) can be accessed into view page
-         * as $name and $id.
+         * as $view_name and $_viewid.
          * This method will include the required phtml file.
          * 
          * @access protected
          * @author Renie Siqueira da Silva
-         * @version 1.0
+         * @version 1.0.1
          * @since 1.0
          * @param String $viewName
-         * @param Array or String $vars
          * @return void
          */
-        protected function view($viewName, $vars = null){
-            if(is_array($vars) && count($vars)>0)
-                extract($vars, EXTR_PREFIX_ALL, 'view');
+        protected function view($viewName){
+            if(count($this->vars)>0)
+                extract($this->vars, EXTR_PREFIX_ALL, 'view');
             
             $pure_controller    = SYSTEM::getPureController();
             $pure_action        = SYSTEM::getPureAction();
             
             return require_once('app/views/'.$viewName.'.phtml');
+        }
+        
+        /**
+         * Receive parameters will be accessed on view.
+         * 
+         * @access protected
+         * @author Renie Siqueira da Silva
+         * @version 1.0.1
+         * @since 1.0.1
+         * @param String $viewName
+         * @return void
+         */
+        protected function setVars($name, $value){
+            $this->vars[$name] = $value;
+            return $this;
         }
         
     }
